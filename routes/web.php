be9694be60d32867b42354af9b8d133c65536731
mediantosaragih001 <medianto.s@literasia.co.id>
2022-04-18 20:11:15 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use App\Http\Controllers\Admin\PengobatanController;
 */
 
 Route::get('/', function () {
-    return view('login');
+    return view('welcome');
 });
 Route::get('/welcome', function () {
     return view('welcome');
