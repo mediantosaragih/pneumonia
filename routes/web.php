b88c0aacd193e5de6ad1b74eb2e5bdda4c3f805d
mediantosaragih001 <medianto.s@literasia.co.id>
2022-05-11 08:02:33 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,10 @@ Route::get('/history', function () {
     return view('admin.history');
 });
 
+Route::get('/cetak-hasil', function () {
+    return view('cetak-hasil');
+});
+
 //Login
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'authenticate']);
